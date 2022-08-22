193a36503e8123fc34ed122ddd3820ccb11cd5a3
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-08-22 11:55:09 -0300

    Implementing header, signIn and subscribe button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const { data: session } = useSession();
+
+    return session ? (
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color='#04d361' />
+            {session.user.name}
+            <FiX color='#737380' className={styles.closeIcon} onClick={() => signOut()} />
+        </button>
+    ) : (
+        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
+            <FaGithub color='#eba417' />
+            Sign in with Github
+        </button>
+    )
+}
