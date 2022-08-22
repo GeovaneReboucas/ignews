8b441e6160428eced04eb31e1cf57cf6c104d2ed
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-08-22 11:53:49 -0300

    Configuring typescript and home application

---
+++ src/services/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(
+    process.env.STRIPE_API_KEY,
+    {
+        apiVersion: '2022-08-01',
+        appInfo: {
+            name: 'Ignews',
+            version: '0.1.0',
+        }
+    }
+)
