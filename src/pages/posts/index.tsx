f48a68d32eb619b34b3fd408d5e34adad0a68f7f
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-08-26 06:34:13 -0300

    Consuming Prismic CMS posts and adjusting header routes

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,7 @@ import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 import * as Prismic from "../../../node_modules/@prismicio/client/dist/index";
+import { RichText } from 'prismic-dom';
 
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient();
@@ -19,14 +20,38 @@ export const getStaticProps: GetStaticProps = async () => {
         }
     )
 
-    console.log(response);
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        }
+    })
 
     return {
-        props: {}
+        props: {
+            posts
+        }
     }
 }
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
+
+interface PostsProps {
+    posts: Post[]
+};
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -35,24 +60,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with lerna & yarn workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared.</p>
-                    </a>
-
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with lerna & yarn workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared.</p>
-                    </a>
-
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with lerna & yarn workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared.</p>
-                    </a>
-
+                    {posts.map(post => (
+                        <a key={post.slug} href='#'>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
