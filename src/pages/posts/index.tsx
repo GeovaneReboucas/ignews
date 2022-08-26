d8b08a7b9dfcf960318898309fbc82b8c2e6d469
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-08-26 08:24:13 -0300

    Creating post page and verifying active subscription

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,47 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { GetStaticProps } from 'next';
 
 import styles from './styles.module.scss';
-
-import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 import * as Prismic from "../../../node_modules/@prismicio/client/dist/index";
 import { RichText } from 'prismic-dom';
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
+
+interface PostsProps {
+    posts: Post[]
+};
+
+export default function Posts({ posts }: PostsProps) {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
+}
 
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient();
@@ -38,37 +73,4 @@ export const getStaticProps: GetStaticProps = async () => {
             posts
         }
     }
-}
-
-type Post = {
-    slug: string;
-    title: string;
-    excerpt: string;
-    updatedAt: string;
-};
-
-interface PostsProps {
-    posts: Post[]
-};
-
-export default function Posts({ posts }: PostsProps) {
-    return (
-        <>
-            <Head>
-                <title>Posts | Ignews</title>
-            </Head>
-
-            <main className={styles.container}>
-                <div className={styles.posts}>
-                    {posts.map(post => (
-                        <a key={post.slug} href='#'>
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
-                    ))}
-                </div>
-            </main>
-        </>
-    );
 }
